60de0efb86a0653d9386098441f25c152ede9a91
tantnguyen <tnguyen@eline.com>
2020-04-05 13:39:26 -0700

    Added mark tasks completed

--- app.js
+++ app.js
@@ -38,6 +38,9 @@ class TasksApp {
     taskComponent.id = `task-${task.id}`;
     taskComponent.setAttribute('data-id', task.id)
     taskComponent.classList.add('task-wrapper');
+    if(!!task.completed) {
+      taskComponent.classList.add('completed');
+    }
 
     let taskContainer = document.createElement('div');
     taskContainer.classList.add('task-container');
@@ -48,14 +51,33 @@ class TasksApp {
     let taskContent = document.createElement('p');
     taskContent.innerText = task.content;
 
-    let taskDate = document.createElement('p');
-    taskDate.innerText = task.date;
+    let dateContainer = document.createElement('div');
+    dateContainer.classList.add('date-container');
+
+    if(task.completed) {
+      let completedDate = document.createElement('span');
+      completedDate.classList.add('completed-date');
+      let dateObj = new Date(task.completed);
+      let date = `${dateObj.getMonth() +1}/${dateObj.getDate()}/${dateObj.getFullYear()}`
+      completedDate.innerHTML = `<span class="check-completed">&check;</span> Completed: ${date}`;
+      dateContainer.append(completedDate);
+    }
+
+    let taskDate = document.createElement('span');
+    taskDate.classList.add('target-date');
+    let dateObj = new Date(task.date);
+    let date = `${dateObj.getMonth() +1}/${dateObj.getDate()}/${dateObj.getFullYear()}`
+    taskDate.innerHTML = `Target: ${date}`;
+    dateContainer.append(taskDate);
+
+    let btnContainer = document.createElement('div');
+    btnContainer.classList.add('btn-container');
 
     let removeBtn = document.createElement('button');
     removeBtn.setAttribute('data-id', task.id);
     removeBtn.classList.add('remove-btn');
     removeBtn.title = 'Remove';
-    removeBtn.innerHTML = '&times;';
+    removeBtn.innerHTML = '&#x1f5d1; Remove';
     removeBtn.addEventListener('click', function(e) {
       _this.removeTask(e)
     })
@@ -65,15 +87,29 @@ class TasksApp {
     markCompletedBtn.classList.add('mark-completed-btn');
     markCompletedBtn.title = 'Mark Completed';
     markCompletedBtn.innerHTML = '&check; Completed';
+    if(!!task.completed) markCompletedBtn.setAttribute('disabled', true);
     markCompletedBtn.addEventListener('click', function(e) {
-      // _this.removeTask(e)
+      _this.markAsCompleted(e);
     })
 
-    taskContainer.append(taskTitle, taskContent, taskDate, markCompletedBtn, removeBtn)
+    btnContainer.append(removeBtn, markCompletedBtn);
+
+    taskContainer.append(taskTitle, dateContainer, taskContent, btnContainer)
     taskComponent.append(taskContainer);
 
     // this.animations.fadeIn(taskComponent);
     return taskComponent;
+  }
+
+  markAsCompleted(e) {
+    const id = e.target.dataset.id;
+    e.target.setAttribute('disabled', true);
+    
+    const wrapper = document.querySelector(`.task-wrapper[data-id="${id}"]`);
+    wrapper.classList.toggle('completed');
+    const date = new Date();
+    this.tasks[id].completed = Date.now();
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
   saveTasks(tasks) {
@@ -119,14 +155,12 @@ class TasksApp {
       const content = document.querySelector('#task-form textarea[name="content"]').value;
       const dateObj = new Date(document.querySelector('#task-form input[name="date"]').value);
       let date = dateObj.getTime();
-      const important = document.querySelector('input[name="important"]').checked;
       let values = 
         { 
           id: Date.now(),
           title,
           content,
           date,
-          important
         }
       _this.addTask(values)
     }
@@ -137,9 +171,6 @@ class TasksApp {
     for(let input of inputs) {
       input.value = '';
     }
-    
-    const checkbox = document.querySelector('#task-form input[type="checkbox"]');
-    checkbox.removeAttribute('checked');
 
     const textarea = document.querySelector('#task-form textarea');
     textarea.value = '';
