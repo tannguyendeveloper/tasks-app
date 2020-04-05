d9af9ec8b74e16eb2783299f7ae2df7f60f41a8c
tantnguyen <tnguyen@eline.com>
2020-04-04 23:17:53 -0700

    First

---
+++ app.js
@@ -0,0 +1,163 @@
+window.addEventListener('load', function () {
+  const App = new TasksApp();
+})
+
+class TasksApp {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    console.log('initializing app');
+    this.renderTasks();
+    this.initForm();
+    this.initUI();
+  }
+
+  getTasks() {
+    console.log('getting tasks');
+    return localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : {};
+  }
+
+  renderTasks() {
+    this.tasks = this.getTasks();
+    const tasksContainer = document.querySelector('#task-container');
+    tasksContainer.innerHTML = ''
+    if(Object.keys(this.tasks).length) {
+      for(let task in this.tasks) {
+        console.log(task)
+        tasksContainer.append(this.taskComponent(this.tasks[task]));
+      }
+    } else {
+      console.log('Has no tasks')
+    }
+  }
+
+  taskComponent(task) {
+    let taskComponent = document.createElement('div');
+    taskComponent.id = `task-${task.id}`;
+    taskComponent.setAttribute('data-id', task.id)
+    taskComponent.classList.add('task-wrapper');
+
+    let taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-container');
+
+    let taskTitle = document.createElement('h2');
+    taskTitle.innerText = task.title;
+
+    let taskContent = document.createElement('p');
+    taskContent.innerText = task.content;
+
+    let taskDate = document.createElement('p');
+    taskDate.innerText = task.date;
+
+    let removeBtn = document.createElement('button');
+    removeBtn.setAttribute('data-id', task.id);
+    removeBtn.classList.add('remove-btn');
+    removeBtn.innerHTML = '&times;'
+
+    taskContainer.append(taskTitle, taskContent, taskDate)
+    taskComponent.append(taskContainer)
+
+    // this.animations.fadeIn(taskComponent);
+    return taskComponent;
+  }
+
+  saveTasks(tasks) {
+    if(Object.keys(tasks).length) {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    }
+  }
+
+  addTask(values) {
+    const tasksContainer = document.querySelector('#task-container');
+    this.tasks[values.id] = values;
+    tasksContainer.append(this.taskComponent(task));
+    this.saveTasks(this.tasks);
+  }
+
+  removeTask() {
+
+  }
+
+  handleFormSubmit(e) {
+    const _this = this;
+    console.log(_this);
+    const form = document.querySelector('#task-form');
+    if(form.checkValidity()) {
+      e.preventDefault();
+      console.log('is valid');
+      const form = document.querySelector('#task-form');
+      const title = document.querySelector('#task-form input[name="title"]').value;
+      const content = document.querySelector('#task-form textarea[name="content"]').value;
+      const dateObj = new Date(document.querySelector('#task-form input[name="date"]').value);
+      let date = dateObj.getTime();
+      const important = document.querySelector('input[name="important"]').checked;
+      let values = 
+        { 
+          id: Date.now(),
+          title,
+          content,
+          date,
+          important
+        }
+      _this.addTask(values)
+    }
+  }
+
+  clearFormInputs() {
+
+  }
+
+  initForm() {
+    const _this = this;
+    const form = document.querySelector('#task-form');
+    const submitBtn = document.querySelector('#task-form button[type="submit"]');
+    submitBtn.addEventListener('click', function(e) { 
+      _this.handleFormSubmit(e); 
+    })
+  }
+
+  initUI() {
+    this.initAddTaskBtn();
+    this.initClosePanelBtn();
+  }
+
+  initAddTaskBtn() {
+    const _this = this;
+    const addTaskBtn =  document.querySelector('#add-task-btn');
+    addTaskBtn.addEventListener('click', async function() {
+      const taskFormPanel = document.querySelector('#task-form-panel');
+      taskFormPanel.classList.remove('hidden');
+      // const panelsAreClosed =_this.closeAllPanels();
+      // if(panelsAreClosed) _this.transitions.slideIn(addTaskBtn);
+    })
+  }
+
+  initClosePanelBtn() {
+    const _this = this;
+    let btns = document.querySelectorAll('.close-panel-btn');
+    for(let btn of btns) {
+      btn.addEventListener('click', function(e) {
+        const panel = e.target.parentElement;
+        panel.classList.add('hidden');
+        _this.clearFormInputs();
+      })
+    }
+  }
+
+
+  openPanel() {
+
+  }
+
+  closePanel() {
+
+  }
+
+  async closeAllPanels() {
+    console.log('closing all open panels');
+    let openPanel = document.querySelectorAll(`.panel.open`);    
+  }
+
+}
