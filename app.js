4694db6c44f80450bc2e40984ba553a497cf247d
tantnguyen <tnguyen@eline.com>
2020-04-05 18:52:56 -0700

    Added filter by select value

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ class TasksApp {
   init() {
     this.renderTasks();
     this.initForm();
+    this.initSelectFilter();
     this.initUI();
   }
 
@@ -25,20 +26,20 @@ class TasksApp {
       for(let task in this.tasks) {
         tasksContainer.append(this.taskComponent(this.tasks[task]));
       }
-    } else {
-      console.log('Has no tasks')
     }
   }
 
   taskComponent(task) {
-    console.log(task);
     const _this = this;
     let taskComponent = document.createElement('div');
+    console.log(task);
     taskComponent.id = `task-${task.id}`;
     taskComponent.setAttribute('data-id', task.id)
     taskComponent.classList.add('task-wrapper');
     if(!!task.completed) {
       taskComponent.classList.add('completed');
+    } else if (task.date < Date.now()) {
+      taskComponent.classList.add('overdue');
     }
 
     let taskContainer = document.createElement('div');
@@ -53,7 +54,7 @@ class TasksApp {
     let dateContainer = document.createElement('div');
     dateContainer.classList.add('date-container');
 
-    if(task.completed) {
+    if(!!task.completed) {
       let completedDate = this.completedDate(task.completed);
       dateContainer.append(completedDate);
     }
@@ -173,11 +174,9 @@ class TasksApp {
 
   handleFormSubmit(e) {
     const _this = this;
-    console.log(_this);
     const form = document.querySelector('#task-form');
     if(form.checkValidity()) {
       e.preventDefault();
-      console.log('is valid');
       const form = document.querySelector('#task-form');
       const title = document.querySelector('#task-form input[name="title"]').value;
       const content = document.querySelector('#task-form textarea[name="content"]').value;
@@ -238,6 +237,96 @@ class TasksApp {
     })
   }
 
+  initSelectFilter() {
+    const select = document.querySelector('#task-filter');
+    const _this = this;
+    select.addEventListener('change', function(e) {
+      const filter = e.target.value;
+      if(filter === 'all') {
+        _this.renderTasks();
+      } else if(filter === 'completed') {
+        let completedTasks = _this.filterCompletedTasks();
+        _this.renderFilteredTasks(completedTasks);
+      } else if(filter === 'overdue') {
+        let overdueTasks = _this.filterOverdueTasks();
+        _this.renderFilteredTasks(overdueTasks);
+      } else if(filter === 'incomplete') {
+        let incompleteTasks = _this.filterIncompleteTasks();
+        _this.renderFilteredTasks(incompleteTasks);
+
+      }
+    })
+  }
+
+  filterCompletedTasks() {
+    let arrOfKeys = Object.keys(this.tasks).filter((key) => {
+      return !!this.tasks[key].completed;
+    })
+    let arrOfTasks = arrOfKeys.map((key) => {
+      return this.tasks[key];
+    })
+    let sortedTasks = arrOfTasks.sort(this.compareByCompletedDate);
+    return sortedTasks;
+  }
+
+  filterOverdueTasks(tasks) {
+    let arrOfKeys = Object.keys(this.tasks).filter((key) => {
+      return this.tasks[key].date < Date.now() && !this.tasks[key].completed;
+    })
+    let arrOfTasks = arrOfKeys.map((key) => {
+      return this.tasks[key];
+    })
+    let sortedTasks = arrOfTasks.sort(this.compareByCompleteByDate);
+    return sortedTasks;
+  }
+
+  filterIncompleteTasks(tasks) {
+    let arrOfKeys = Object.keys(this.tasks).filter((key) => {
+      return !this.tasks[key].completed;
+    })
+    let arrOfTasks = arrOfKeys.map((key) => {
+      return this.tasks[key];
+    })
+    let sortedTasks = arrOfTasks.sort(this.compareByCompleteByDate);
+    return sortedTasks;
+  }
+
+  renderFilteredTasks(tasks) {
+    const tasksContainer = document.querySelector('#tasks-container');
+    tasksContainer.innerHTML = ''
+    if(tasks.length) {
+      for(let task of tasks) {
+        tasksContainer.append(this.taskComponent(task));
+      }
+    }
+  }
+
+  compareByCompletedDate(a, b) {
+    // Use toUpperCase() to ignore character casing
+    const x = a.completed;
+    const y = b.completed;
+    let comparison = 0;
+    if (x > y) {
+      comparison = 1;
+    } else if (x < y) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+
+  compareByCompleteByDate(a, b) {
+    // Use toUpperCase() to ignore character casing
+    const x = a.date;
+    const y = b.date;
+    let comparison = 0;
+    if (x > y) {
+      comparison = 1;
+    } else if (x < y) {
+      comparison = -1;
+    }
+    return comparison;
+  }
+
 
   initClosePanelBtn() {
     const _this = this;
@@ -277,7 +366,6 @@ class TasksApp {
       el.classList.add('slide-in');
       el.classList.remove('slide-out');
       const animation = setInterval(function() {
-        console.log('adding hidden');
         el.classList.add('hidden');
         clearInterval(animation);
         return true;
